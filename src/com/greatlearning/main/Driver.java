f2c78f1babe9c8fd75e8debdbedc929ebee64102
SakinaKothawala <89722949+SakinaKothawala@users.noreply.github.com>
2021-08-29 16:08:18 +0530

    Lab1 solution

---
+++ src/com/greatlearning/main/Driver.java
@@ -0,0 +1,52 @@
+package com.greatlearning.main;
+import java.util.Scanner;
+import com.greatlearning.model.Employee;
+import com.greatlearning.service.CredentialService;
+
+public class Driver {
+	private static final Scanner sc = new Scanner( System.in );
+	
+	
+	public static void main( String[] args ) {
+		System.out.println( "Enter First name : " );
+		String firstName = sc.next();
+		
+		System.out.println( "Enter Last name : " );
+		String lastName = sc.next();
+		
+		System.out.println( "Kindly Choose your department : " );
+		System.out.println( "1. Technical" );
+		System.out.println( "2. Admin" );
+		System.out.println( "3. Human Resources" );
+		System.out.println( "4. Legal" );
+		
+		int choice = sc.nextInt();
+	
+	    Employee emp;
+		
+		switch( choice ) {
+			case 1:
+				emp = new Employee( firstName, lastName, "tech" );
+				break;
+			case 2:
+				emp = new Employee( firstName, lastName, "adm" );
+				break;
+			case 3:
+				emp = new Employee( firstName, lastName, "hr" );
+				break;
+			case 4:
+				emp = new Employee( firstName, lastName, "lg" );
+				break;
+			default:
+				System.out.println( "Incorrect choice" );
+				return;
+		}
+		
+		CredentialService cs = new CredentialService();
+		
+		String email = cs.generateEmailAddress( emp );
+		String password = cs.generatePassword();
+		
+		cs.showCredentials( emp, email, password );
+	}
+}
